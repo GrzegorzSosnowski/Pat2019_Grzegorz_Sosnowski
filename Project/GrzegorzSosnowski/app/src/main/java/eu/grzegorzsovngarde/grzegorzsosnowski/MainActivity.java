9e5435e8e94110b098cdf0c8ef06f17d2dc791c6
GrzegorzSosnowski <grzegorz.sosnowski94@gmail.com>
2019-01-18 22:18:53 +0100

    Added login/password validation on button click, set text on incorrect data

--- Project/GrzegorzSosnowski/app/src/main/java/eu/grzegorzsovngarde/grzegorzsosnowski/MainActivity.java
+++ Project/GrzegorzSosnowski/app/src/main/java/eu/grzegorzsovngarde/grzegorzsosnowski/MainActivity.java
@@ -7,15 +7,22 @@ import android.view.View;
 
 import android.widget.EditText;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
 public class MainActivity extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Load layout for onCreate instance
         setContentView(R.layout.activity_main);
-        //EditText editText = (EditText)findViewById(R.id.editText);
-        //EditText editText2 = (EditText)findViewById(R.id.editText2);
         findViewById(R.id.button);
     }
 
@@ -25,9 +32,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View v) {
-        Intent mainScreenIntent = new Intent(this,Mainscreen.class);
-        this.startActivity(mainScreenIntent);
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        String login = editText.getText().toString();
+        final EditText editText2 = (EditText) findViewById(R.id.editText2);
+        String password = editText2.getText().toString();
 
+        String lPATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        String pPATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$";
+        Pattern lpattern = Pattern.compile(lPATTERN);
+        Pattern ppattern = Pattern.compile(pPATTERN);
+        Matcher lmatcher = lpattern.matcher(login);
+        Matcher pmatcher = ppattern.matcher(password);
+        boolean lvalid = lmatcher.matches();
+        boolean pvalid = pmatcher.matches();
+        if(!lvalid||!pvalid) {
+            editText.setText(getString(R.string.incorrect_credentials));
+        }
+        else {
+            Intent mainScreenIntent = new Intent(this, Mainscreen.class);
+            this.startActivity(mainScreenIntent);
+        }
     }
 }
 
